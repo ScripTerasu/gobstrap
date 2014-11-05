598083366e228d5038fbfca7c5c90d0a320fd909
david s <messthetic@gmail.com>
2014-11-05 19:37:01 -0300

    Añade js que oculta logo en .header-gobcl

--- dist/js/gobstrap.js
+++ dist/js/gobstrap.js
@@ -2170,3 +2170,33 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   })
 
 }(jQuery);
+
+/* ========================================================================
+ * Gobstrap: custom.js v3.2.0
+ * http://getbootstrap.com/javascript/#affix
+ * ========================================================================
+ * Copyright 2011-2014 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
+
+
++function ($) {
+  'use strict';
+
+  // $(".header-gobcl").button('toggle').addClass('hola')
+
+
+    $('.navbar-collapse').on('shown.bs.collapse', function (e) {
+         $('.header-gobcl #logo-main').addClass('off');
+         $('.header-gobcl #logo-main').removeClass('on');
+         
+    });
+
+    $('.navbar-collapse').on('hidden.bs.collapse', function (e) {
+        $('.header-gobcl #logo-main').addClass('on');
+         $('.header-gobcl #logo-main').removeClass('off');
+        
+    });
+
+
+}(jQuery);
